15ba70820f96893f801c81069451a62919c9c03d
idreaming <deming@buaa.edu.cn>
2020-12-06 00:19:46 +0800

    [add]:Add flow configuration interface

---
+++ src/core/flowcfg.h
@@ -0,0 +1,16 @@
+#pragma once
+namespace anyflow {
+class flowcfg{
+public:
+    typedef enum{
+        sync,
+        async,
+        microsync,
+    } run_mode;
+    inline flowcfg(){}
+    inline flowcfg(run_mode mode,run_mode cb_mode):m_mode(mode),m_cb_mode(cb_mode){}
+    inline virtual ~flowcfg(){}
+    run_mode m_mode;
+    run_mode m_cb_mode;
+};
+}
